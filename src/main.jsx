c5f9f89c5d713df11cc38fd5f6ad1c35e6a714ed
Emil Fridriksson <emil@millicentral.com>
2025-02-04 10:28:40 +0000

    Add proper Vite project structure

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TimeTracker from './TimeTracker'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TimeTracker />
+  </React.StrictMode>,
+)
